54959b1a2c474674bb6019c4a32a5f31c84bd57c
Jiri Kuncar <jiri.kuncar@gmail.com>
2018-09-27 13:29:14 +0200

    Renku run command

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,14 @@ import {
   JupyterLab, JupyterLabPlugin
 } from '@jupyterlab/application';
 
+import {
+  ICommandPalette
+} from '@jupyterlab/apputils';
+
+import {
+  Widget
+} from '@phosphor/widgets';
+
 import '../style/index.css';
 
 
@@ -11,8 +19,33 @@ import '../style/index.css';
 const extension: JupyterLabPlugin<void> = {
   id: 'renku-jupyterlab-ts',
   autoStart: true,
-  activate: (app: JupyterLab) => {
+  requires: [ICommandPalette],
+  activate: (app: JupyterLab, palette: ICommandPalette) => {
     console.log('JupyterLab extension renku-jupyterlab-ts is activated!');
+    console.log('ICommandPalette:', palette);
+
+    // Create a single widget
+    let widget: Widget = new Widget();
+    widget.id = 'renku-run';
+    widget.title.label = 'renku run';
+    widget.title.closable = true;
+
+    // Add an application command
+    const command: string = 'renku:run';
+    app.commands.addCommand(command, {
+      label: 'Run notebook with Renku',
+      execute: () => {
+        if (!widget.isAttached) {
+          // Attach the widget to the main work area if it's not there
+          app.shell.addToMainArea(widget);
+        }
+        // Activate the widget
+        app.shell.activateById(widget.id);
+      }
+    });
+
+    // Add the command to the palette.
+    palette.addItem({command, category: 'Renku'});
   }
 };
 
